9b1c7a35e58acc8c7f4344432b7c30aaaa8b2d0e
Yandro Portal <suarez.portal@gmail.com>
2021-04-07 22:08:04 -0500

    Add files via upload

---
+++ mergeSort.js
@@ -0,0 +1,57 @@
+//Time Complexity: Merge Sort runs at O(n log n) at both best and worst case scenarios.
+
+/**Merge Sort is always a logarithmic sorting algorithm; dividing the array in two
+halves* and takes linear time to merge the two arrays*/
+
+// merging function
+function merge(arr1, arr2){
+    let result = []; 
+    let i = 0; 
+    let j = 0;
+  
+  
+  
+    while(i < arr1.length && j < arr2.length){
+
+      if(arr1[i] > arr2[j]) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+  
+    while(i < arr1.length){
+      result.push(arr1[i]);
+      i++;
+    }
+  
+    while(j < arr2.length){
+      result.push(arr2[j]);
+      j++;
+    }
+  
+    return result;
+  }
+  
+  function mergeSort(arr){
+
+    if(arr.length <= 1) return arr;
+  
+    let middle = Math.ceil(arr.length / 2);
+  
+    let left = mergeSort(arr.splice(0, middle));
+  
+  
+    let right = mergeSort(arr.splice(-middle));
+  
+  
+    return merge( left, right);
+  }
+  
+  const arr = [50, 80, 100, 25, 18, 10, 1, 2, 3, 5, 6 , 12, 15, 22, 90];
+  
+  console.log(mergeSort(arr));
+
+  //[1, 2, 3, 5, 6, 10, 12, 15, 18, 22, 25, 50, 80, 90, 100]
