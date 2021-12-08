4452922ead2842af724a007293ef63a0d26f4414
Yandro Portal <suarez.portal@gmail.com>
2021-12-08 17:29:00 -0600

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -10,9 +10,9 @@
     for(let i = 0 ; i < len; i++){
         for(let j = 0; j < len; j++){
             if(arr[j] > arr[j + 1]){
-                let temp = arr[j];
+                let swap = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = temp;    // if the first element is greater than the second, a swap will occur to place the smaller element first
+                arr[j + 1] = swap;    // if the first element is greater than the second, a swap will occur to place the smaller element first
             }
     
         }
